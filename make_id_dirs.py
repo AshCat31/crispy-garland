3628c5f5d9e7e15b71f176d4a33bcaeea118fa32
AshCat31 <31ccat520@gmail.com>
2024-06-26 15:49:55 -0700

    make_id_dirs

---
+++ make_id_dirs.py
@@ -0,0 +1,14 @@
+import os
+
+device_file = '/home/canyon/Test_Equipment/crispy-garland/QA_ids.txt'
+path = '/home/canyon/S3bucket'
+
+with open(device_file, 'r') as file:
+    for line in file:
+        folder_name = line.split()[0].strip()
+        folderpath = os.path.join(path, folder_name, "calculated_transforms",folder_name)
+        if os.path.exists(folderpath):
+            print(f"Folder already exists: {folderpath}")
+        else:
+            os.makedirs(folderpath)
+            print(f"Created folder: {folderpath}")

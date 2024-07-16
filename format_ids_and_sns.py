d7389b7dda44d0275169c1f2d8c7120861f9577a
AshCat31 <31ccat520@gmail.com>
2024-07-16 13:18:33 -0700

    mutmut stuff

--- format_ids_and_sns.py
+++ format_ids_and_sns.py
@@ -1,13 +1,16 @@
-def transform_and_overwrite(input_file):
+def overwrite(input_file):
     """Reformats scanned QA_ids for Gsheet"""
     with open(input_file, 'r') as f:
         lines = f.read().strip().splitlines()
 
-    pairs = ['\t'.join(lines[i:i+2]) for i in range(0, len(lines), 2)]
-    
+    pairs = transform(lines)
+
     with open(input_file, 'w') as f:
         f.write('\n'.join(pairs) + '\n')
 
+def transform(lines):
+    return ['\t'.join(lines[i:i+2]) for i in range(0, len(lines), 2)]
+
 if __name__ == "__main__":
     input_file = '/home/canyon/Test_Equipment/crispy-garland/QA_ids.txt'
-    transform_and_overwrite(input_file)
+    overwrite(input_file)
--- mutmut_config.py
+++ mutmut_config.py
@@ -4,7 +4,4 @@ import os.path
 def pre_mutation(context):
     dirname, filename = os.path.split(context.filename)
     testfile = "test_" + filename
-    print(context.config.test_command)
-    context.config.test_command += ' ' + os.path.join('tests', dirname, testfile)
-    # print(dirname, testfile)
-    # print(context.config.test_command)
+    context.config.test_command += ' ' + os.path.join('tests', 'unit', dirname, testfile)
